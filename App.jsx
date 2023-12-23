ecebb64762484a023f5245512e9c40bd21969294
mertertul <151396608+mertertul@users.noreply.github.com>
2023-12-23 11:13:59 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+<h1> BEN KİMİM HAKKIMDA </h1> 
+<p>2004 yılında Türkiye'nin Konya İlinde doğdu.</p>
+<div>İlköğrenimini 1,2ve3. sınıfları //// İlköğretim okulunda tamamlarken 4.sınıf'ı ise Konya Selçuklu Sancak Mahallesi Şehit Osman Demir İlkokulunda tamamlamıştır.</div>
+<p>Ortaöğretimin ilk parçası Ortaokul öğrenimini ise 5,6,7,8. Sınıf olarak //// Ortaokulunda tamamlamıştır.</p>
+<div>
+Ortaöğretimin ikinci parçası olan Lise Öğrenimini /////**/ 9,10,11.sınıfları okuyarak 12.sınıf öğreniminde ise Açıköğretim Merkeze Bağlı Taşra eğitim kurumlarında okuyarak lise öğrenimini tamamlayıp mezun olmuştur.
+
+Şuan da çocukluk yıllarından beri hayali olan bilgisayar ve teknoloji alanında yapmak istediği çalışmalar onu şuan aldığı eğitimlere sürüklemiş olmakla birlikte üniversite ön hazırlık olarak yazılım alanında programlama eğitimleri almaya devam ediyor.
+
+Bu site web teknolojileri için ilk seviye bir web sitesi olup siz değerli kullanıcılarımıza arz ederiz,herkese bol başarılı iyi çalışmalar. :)
+</div>
+
+<img src="public/yazılılmcı genç student free .PNG"></img>
+
+<span>Ben Ne Yapıyorum:</span>
+<p>Ben türkiye de yaşayan bir genç olarak ilme ve teknolojiye hayaranlık duyuyorum.</p>
+<ul style={style-tyepe:"none"}>
+<li>CSS ÖĞRENMEYE ÇALIŞIYORUM</li>
+<li>HTML ÖĞRENMEYE ÇALIŞIYORUM</li>
+<li>JAVASCRİPT ÖĞRENMEYE ÇALIŞIYORUM</li>
+<li>REACT.JS KÜTÜPHANESİNİ ÖĞRENMEYE ÇALIŞIYORUM</li>
+<li>EXCEL MİCROSOFOFT ÖĞRENMEYE ÇALIŞIYORUM</li>
+<li>WORD MİCROSOFOFT ÖĞRENMEYE ÇALIŞIYORUM</li>
+<li>POWERPOİNT MİCROSOFOFT ÖĞRENMEYE ÇALIŞIYORUM</li>
+<li>İNGİLİZCE A1/A2 ĞRENMEYE ÇALIŞIYORUM</li>
+<li>MATEMATİK TURKEY 9/10/11/12 ĞRENMEYE ÇALIŞIYORUM</li>
+<li>GEOMETRİ TURKEY 9/10/11/12 ĞRENMEYE ÇALIŞIYORUM</li>
+<li>URETKEN ACADEMY DE OGRENCİLİK YAPIYORUM</li>
+<li>UNİVERSTY DE PROGRAMİNG OGRENİYORUM</li>
+<li>C DİLİ ÇOK İYİ ÖĞRENMEM LAZIM</li>
+<li>WEB SİTESİ YAPMAYI ÇOK İYİ BİLMEM LAZIM</li>
+<li>ALGORİTMA YAZMAYI ÇOK İYİ BİLMEM LAZIM</li>
+<li>EDEBİYAT YANİ DİL BİLGİSİ HAKKINDA EN AZ 20 KİTAP BİTİRMEK LAZIM GELİYOR..</li>
+</ul>
+
+        <a href="App.jsx">Anasayfa</a> |
+        <a href="cv.jsx">Özgeçmiş</a> |
+        <a href="kitaplar.jsx">Kitaplarım</a> |
+        <a href="iletişim.jsx">İletişim</a> |
+        <br>
+        <hr>
+   
+   
+
+
+
+
+    </>
+  )
+}
+
+export default App
+
+
+
+
---
+++ cv.jsx
@@ -0,0 +1,6 @@
+<a href="App.jsx">Anasayfa</a> |
+<a href="cv.jsx">Özgeçmiş</a> |
+<a href="kitaplar.jsx">Kitaplarım</a> |
+<a href="iletişim.jsx">İletişim</a> |
+<br>
+<hr>
